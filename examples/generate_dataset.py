0155f0bb61050283b850bbc1484db4f57551f2cf
vruetten <vms.ruetten@gmail.com>
2025-05-28 09:55:25 -0400

    add random connectivity

--- examples/generate_dataset.py
+++ examples/generate_dataset.py
@@ -21,27 +21,30 @@ np.random.seed(0)
 
 # Parameters for scaling
 
-N_poisson = 10  # Number of Poisson nodes
+N_poisson = 4  # Number of Poisson nodes
 
 N_exponential = 20  # Number of exponential nodes
 N_exponential_groups = 3 # Number of groups of exponential nodes 
 
-N_oscillator = 0  # Number of oscillator nodes
+N_oscillator = 10  # Number of oscillator nodes
+N_oscillator_groups = 1 # Number of groups of oscillator nodes
 
-N_gaussian_kernel = 20  # Number of gaussian kernel nodes
+N_gaussian_kernel = 0  # Number of gaussian kernel nodes
 N_gaussian_kernel_groups = 3 # Number of groups of gaussian kernel nodes
 
 N_damped_oscillation = 0  # Number of damped oscillation nodes
 N_damped_oscillation_groups = 3 # Number of groups of damped oscillation nodes
 
 N_bump = 0  # Number of bump nodes
-N_alpha = 0  # Number of alpha nodes
+
+N_alpha = 20  # Number of alpha nodes
+N_alpha_groups = 3 # Number of groups of alpha nodes
 N_biexponential = 0  # Number of biexponential nodes
 
 
 kernel_duration = 3
 
-T = 300  # Simulation duration
+T = 400  # Simulation duration
 DT = 0.01  # Time step
 
 if N_poisson > 0:
@@ -49,31 +52,33 @@ if N_poisson > 0:
 
 if N_exponential > 0:
     # exponential_taus = np.linspace(0.4, 10, N_exponential)*0+0.5
-    exponential_tau_groups = np.linspace(1, 10, N_exponential_groups) # this is used in A - smaller the slow it will decay
-    exponential_taus = np.random.choice(exponential_tau_groups, size=N_exponential) + np.random.randn(N_exponential)*0.001
+    exponential_tau_groups = np.linspace(0.1, 5, N_exponential_groups)  # smaller the faster the decay
+    exponential_taus = np.random.choice(exponential_tau_groups, size=N_exponential)
     # exponential_taus = np.linspace(0.1, 10, N_exponential)
     exponential_taus_A = exponential_taus.copy()
-    exponential_taus = exponential_taus*0 + 1
+    # exponential_taus = exponential_taus*0 + 1
     exponential_noise_levels = np.linspace(0.01, 0.02, N_exponential)*0 + 0.00
 
 if N_oscillator > 0:
-    oscillator_taus = np.linspace(0.1, 0.5, N_oscillator)
-    oscillator_noise_levels = np.linspace(0.01, 0.05, N_oscillator)
-    oscillator_freqs = np.linspace(0.1, 0.5, N_oscillator)
+    oscillator_taus_groups = np.linspace(0.25, 0.35, N_oscillator_groups)
+    # *0 + 0.2 # smaller the faster decay
+    oscillator_types = np.random.choice(np.arange(N_oscillator_groups), size=N_oscillator)
+    oscillator_taus = oscillator_taus_groups[oscillator_types]
+
+    oscillator_noise_levels = np.linspace(0.01, 0.05, N_oscillator)*0
+    oscillator_freqs_groups = np.linspace(1, 3, N_oscillator_groups)*0 + 1
+    oscillator_freqs = oscillator_freqs_groups[oscillator_types]
 
 if N_gaussian_kernel > 0:
     gaussian_noise_levels = np.linspace(0.01, 0.05, N_gaussian_kernel)*0 + 0.0
     gaussian_taus = np.linspace(0.1, 0.5, N_gaussian_kernel)*0 + 0.2
-
     gaussian_delay_groups = np.linspace(0.7, 2.5, N_gaussian_kernel_groups)
     gaussian_delays = np.random.choice(gaussian_delay_groups, size=N_gaussian_kernel)
-
     gaussian_kernels = [kernels.Kernels.gaussian_kernel(sigma=gaussian_taus[i], delay=gaussian_delays[i], dt=DT, duration=kernel_duration)[0] for i in range(N_gaussian_kernel)]
 
 if N_damped_oscillation > 0:
     damped_oscillation_taus = np.linspace(0.1, 1, N_damped_oscillation)*0 + 0.1
     damped_oscillation_noise_levels = np.linspace(0.01, 0.05, N_damped_oscillation)*0 + 0.0
-    
     damped_oscillation_freq_groups = np.linspace(0.5, 0.6, N_damped_oscillation_groups)
     damped_oscillation_freqs = np.random.choice(damped_oscillation_freq_groups, size=N_damped_oscillation)
 
@@ -88,9 +93,14 @@ if N_bump > 0:
     bump_kernels = [kernels.Kernels.bump_kernel(tau_rise=bump_taus[i], tau_sustain=bump_taus[i], tau_decay=bump_taus[i], delay=bump_delays[i], dt=DT, duration=kernel_duration)[0] for i in range(N_bump)]
 
 if N_alpha > 0:
-    alpha_taus = np.linspace(0.1, 0.5, N_alpha)
-    alpha_noise_levels = np.linspace(0.01, 0.05, N_alpha)
-    alpha_delays = np.linspace(0.1, 0.5, N_alpha)
+    alpha_taus_groups = np.linspace(0.1, 0.5, N_alpha_groups)
+    alpha_types = np.random.choice(np.arange(N_alpha_groups), size=N_alpha)
+    alpha_taus = alpha_taus_groups[alpha_types]
+    # alpha_taus_A = alpha_taus.copy()
+    alpha_taus = alpha_taus
+    alpha_noise_levels = np.linspace(0.01, 0.05, N_alpha)*0
+    alpha_delays_groups = np.linspace(0.1, 0.5, N_alpha_groups)
+    alpha_delays = alpha_delays_groups[alpha_types]
     alpha_kernels = [kernels.Kernels.alpha_function(tau=alpha_taus[i], delay=alpha_delays[i], dt=DT, duration=kernel_duration)[0] for i in range(N_alpha)]
 
 if N_biexponential > 0:
@@ -130,7 +140,7 @@ for i in range(N_poisson):
 for i in range(N_exponential):
     node = nodes.ExponentialNode("Exponential", tau=exponential_taus[i], noise_level=exponential_noise_levels[i])
     if i == 0:
-        node.initial_state = np.array([1.0])*1
+        node.initial_state = np.random.randn()*0
     else:
         node.initial_state = np.random.randn()*0
     net.add_node(node)
@@ -141,8 +151,9 @@ for i in range(N_exponential):
 for i in range(N_oscillator):
     if i == 0:
         initial_state = np.random.randn()
+        initial_state = initial_state*0 
     else:
-        initial_state = np.random.randn()
+        initial_state = np.random.randn()*0
     node = nodes.OscillatorNode("Oscillator", tau=oscillator_taus[i], freq=oscillator_freqs[i], initial_state=initial_state)
     net.add_node(node)
     node_types.append("Oscillator")
@@ -151,6 +162,7 @@ for i in range(N_oscillator):
 # --- Create M Gaussian kernel nodes ---
 for i in range(N_gaussian_kernel):
     node = nodes.FilteredExponentialNode(f"Gaussian_{i+1}", tau=gaussian_taus[i], filter_kernel=gaussian_kernels[i], dt=DT, noise_level=gaussian_noise_levels[i])
+    node.initial_state = np.random.randn()*0
     net.add_node(node)
     node_types.append("Gaussian")
     print(f"adding gaussian node {i+1} of {N_gaussian_kernel} - total nodes: {net.n_nodes}")
@@ -210,29 +222,41 @@ biexponential_nodes_idx = np.where(np.array(node_types) == "Biexponential")[0]
 print(node_types)
 
 for i, node_type in enumerate(np.unique(node_types)):
+     same_type_nodes = np.where(np.array(node_types) == node_type)[0]
      if node_type == "Poisson":
          pass
      elif node_type == "Oscillator":
-        pass       
-         
-     else:
+         i = 0
+         for i in range(3):
+             poisson_node_idx = np.random.choice(poisson_nodes_idx)
+             conn[same_type_nodes[i], poisson_node_idx] = 1
+     if node_type != "Poisson":
          # find all nodes of the same type
-        same_type_nodes = np.where(np.array(node_types) == node_type)[0]
-        for i in range(3):
+        for i in range(1):
+            oscillator_node_idx = np.random.choice(oscillator_nodes_idx)
+            if oscillator_node_idx != same_type_nodes[i]:
+                conn[same_type_nodes[i], oscillator_node_idx] = 1
             poisson_node_idx = np.random.choice(poisson_nodes_idx)
-            conn[same_type_nodes[i], poisson_node_idx] = 1.0
+        for i in range(1):
+            conn[same_type_nodes[np.random.randint(0, len(same_type_nodes))], poisson_node_idx] = 1
+        #     
         # Create random stable connectivity matrix
         n_nodes = len(same_type_nodes)
-        M_oscillatory_components = 0
-        oscillation_decay_rate = 10
-        osc_frequencies_b = [3, 0.2, 0.3, 0.4]
-        if node_type == "Exponential":
-            real_eigenvalues = -exponential_taus_A
-        else:
-            real_eigenvalues = -np.linspace(0.1, 1, n_nodes)
-        A, Q, D = connectivity.create_matrix_with_oscillations(n_nodes, M_oscillatory_components=M_oscillatory_components, oscillation_decay_rate=oscillation_decay_rate, osc_frequencies_b = osc_frequencies_b, real_eigenvalues = real_eigenvalues)
+        # M_oscillatory_components = 0
+        # oscillation_decay_rate = 10
+        # osc_frequencies_b = [3, 0.2, 0.3, 0.4]
+        A = np.random.randn(n_nodes, n_nodes)/np.sqrt(n_total)
+        A = A - np.diag(np.diag(A))
         conn[np.ix_(same_type_nodes, same_type_nodes)] = A
 
+        # A = A - np.diag(np.sum(A, axis=1))
+        # real_eigenvalues = -np.linspace(0.1, 1, n_nodes)
+        # if node_type == "Exponential":
+        #     real_eigenvalues = -exponential_taus_A
+        # else:
+        #     real_eigenvalues = -np.linspace(0.1, 1, n_nodes)
+        # A, Q, D = connectivity.create_matrix_with_oscillations(n_nodes, M_oscillatory_components=M_oscillatory_components, oscillation_decay_rate=oscillation_decay_rate, osc_frequencies_b = osc_frequencies_b, real_eigenvalues = real_eigenvalues)
+        
 
 
 
@@ -263,11 +287,12 @@ plotting.plot_results(t, y, labels, net)
 # raster_plot.savefig(f"{base_filename}_raster.png")
 y_norm = (y-y.mean(axis=0))/y.std(axis=0)
 pl.figure()
-pl.imshow(y_norm, extent=[0, T, 0, n_total], cmap="gray")
+pl.imshow(y_norm, extent=[0, T, 0, n_total], cmap="gray", aspect="auto")
 # pl.colorbar()
 pl.show()
 # %%
 
+pl.matshow(y_norm[:,:100])
 #%%
 reload(saving)
 # --- Save results ---
@@ -277,7 +302,6 @@ base_filename = os.path.join(output_dir, "network")
 metadata_df, activity_df = saving.save_network_data(net, y, t)
 metadata_df.to_csv(f"{base_filename}_metadata.csv", index=False)
 activity_df.to_csv(f"{base_filename}_activity.csv", index=False)
-
 print(f"Simulation completed. Results saved in: {output_dir}")
 
 
